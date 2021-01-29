cbf61e318888edb517acd46120d8e7c1856d0a21
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-01-29 10:55:43 +0100

    Added question cards

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const card = document.querySelectorAll('.card__inner');
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', function () {
+        card[i].classList.toggle('is-flipped');
+    });
+}
